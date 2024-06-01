66935108f34b496cce892eba0aa3d8aaa974f016
atay777 <kylychbekkv@gmail.com>
2024-06-01 12:06:25 +0600

    Version 1.1

---
+++ Aktan/app/src/main/java/com/atayc/aktan/GameActvity.java
@@ -0,0 +1,200 @@
+package com.atayc.aktan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GameActvity extends AppCompatActivity {
+
+    private EditText inputName;
+    private Button submitButton;
+    private String playerName = "";
+    TextView speech;
+    ImageView aktan;
+    MediaPlayer at;
+    MediaPlayer background;
+    private int textIndex = 0;
+    private String[] texts = {
+            "Сенин атың ким?",
+            "Таанышканыма кубанычтамын.",
+            "Сен бул жерде жаңы болушуң керек.",
+            "Биздин мекенибиз жөнүндө бир аз айтып берүү мен үчүн чоң сыймык.",
+            "Кыргызстан - асман тиреген тоолордун жана кең, дөңсөөлүү жайыттардын өлкөсү.",
+            "Биздин үй деп атаган аймак Ысык-Көл өрөөнү деп аталат, дүйнөдөгү экинчи чоң тоо көлү болгон кооз Ысык-Көлдүн үйү.",
+            "Ысык-Көл кыргыз эли үчүн ыйык жер. Анын аты биздин тилде Жылуу көл дегенди билдирет, анткени суу эң катуу кышта да толук муздабайт.",
+            "Биздин эл кылымдар бою ушул тоолордо көчмөн малчылар болуп жашап келген, жайкы жана кышкы жайыттардын ортосунда көчүп жүрөбүз.",
+            "Биз кой, эчки жана жылкы оторлорубузга кам көрөбүз, алардан жүн, эт жана сүт алабыз - биздин салттуу жашоо образдарыбыздын негизги азыктары.",
+            "Малдарыбыздан тышкары, Ысык-Көл аймагы өзүнүн мол жаратылыш байлыктары менен белгилүү.",
+            "Биз ошондой эле түшүмдүү өрөөндөрдө арпа, буудай жана жашылчаларды өстүрөбүз.",
+            "Манас кыргыз элинин эң белгилүү жана урматтуу эпикалык каарманы болуп саналат.",
+            "Манас эпосунун башкы каарманы. Манас кыргыз урууларынын башын бириктирип, эл-жеринин биримдигин жана эркиндигин коргоо үчүн күрөшкөн.",
+            "Ал ар түрдүү жоо менен согушуп, кыргыз элинин даңкын арттырган баатыр катары белгилүү.",
+            "Манас эпосу кыргыз улуттук аң-сезиминин жана маданиятынын маанилүү бөлүгү болуп саналат"
+
+    };
+    private MediaPlayer mediaPlayer;
+    // Array of raw resource IDs for audio files
+    private int[] audioResIds = {
+            0,
+            R.raw.nicetomeetu,
+            R.raw.yourenew,
+            R.raw.honor,
+            R.raw.kyrgyzstan,
+            R.raw.kyrgyzstan2,
+            R.raw.kyrgyzstan3,
+            R.raw.kyrgyzstan4,
+            R.raw.kyrgyzstan5,
+            R.raw.kyrgyzstan6,
+            R.raw.kyrgyzstan7,
+            R.raw.manas,
+            R.raw.manas2,
+            R.raw.manas3,
+            R.raw.manas4
+
+
+};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_actvity);
+
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.black));
+
+
+        speech = findViewById(R.id.speechTV);
+        inputName = findViewById(R.id.inputName);
+        submitButton = findViewById(R.id.submitButton);
+        aktan = findViewById(R.id.charAI);
+
+        background = MediaPlayer.create(this, R.raw.spirit);
+
+        // Set an OnTouchListener on the root view of the layout
+        View rootView = findViewById(android.R.id.content);
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (textIndex < texts.length) {
+                        if (texts[textIndex].equals("Сенин атың ким?")) {
+                            // Play the audio for the question
+                            at = MediaPlayer.create(GameActvity.this, R.raw.name);
+                            if (at != null) {
+                                at.start();
+                            }
+                            showInputField();
+                        } else {
+                            updateStory();
+                        }
+                    }
+                }
+                return true;
+            }
+        });
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerName = inputName.getText().toString();
+                if (!playerName.isEmpty()) {
+                    inputName.setVisibility(View.GONE);
+                    submitButton.setVisibility(View.GONE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            textIndex++;
+                            updateStory();
+                        }
+                    }, 0); // 2 second delay
+                } else {
+                    Toast.makeText(GameActvity.this, "Сенин атың ким?", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void updateStory() {
+        if (textIndex < texts.length) {
+            speech.setText(texts[textIndex]);
+            Log.d("PlayActivity", "Displaying text: " + texts[textIndex]);
+
+            // Change the image based on the current text index
+            switch (textIndex) {
+                case 0:
+                    aktan.setImageResource(R.drawable.aktan);
+                    break;
+                case 1:
+                    aktan.setImageResource(R.drawable.aktan);
+                    break;
+                case 2:
+                    aktan.setImageResource(R.drawable.aktan);
+                    break;
+                case 3:
+                    aktan.setImageResource(R.drawable.aktan);
+                    break;
+                case 4:
+                    aktan.setImageResource(R.drawable.aktan);
+                case 5:
+                    aktan.setImageResource(R.drawable.aktan);
+                case 6:
+                    aktan.setImageResource(R.drawable.aktan);
+                    break;
+                case 7:
+                    aktan.setImageResource(R.drawable.aktan);
+                    break;
+                case 8:
+                    aktan.setImageResource(R.drawable.aktan);
+                    break;
+                case 9:
+                    aktan.setImageResource(R.drawable.aktan);
+                case 10:
+                    aktan.setImageResource(R.drawable.aktan);
+                    break;
+                case 11:
+                    aktan.setImageResource(R.drawable.akylai);
+                    break;
+                case 12:
+                    aktan.setImageResource(R.drawable.akylai);
+                    break;
+            }
+
+            // Release any previous media player instance
+            if (mediaPlayer != null) {
+                mediaPlayer.release();
+            }
+
+            // Play the corresponding audio if available
+            if (audioResIds[textIndex] != 0) {
+                mediaPlayer = MediaPlayer.create(GameActvity.this, audioResIds[textIndex]);
+                if (mediaPlayer != null) {
+                    mediaPlayer.start();
+                    Log.d("PlayActivity", "Playing audio: " + audioResIds[textIndex]);
+                } else {
+                    Log.e("PlayActivity", "Failed to create MediaPlayer for audio: " + audioResIds[textIndex]);
+                }
+            }
+
+            textIndex++;
+        } else {
+            speech.setText("The End");
+            Log.d("PlayActivity", "Displaying text: The End");
+        }
+    }
+    private void showInputField() {
+        speech.setText(texts[textIndex]);
+        inputName.setVisibility(View.VISIBLE);
+        submitButton.setVisibility(View.VISIBLE);
+    }
+}
---
+++ Aktan/app/src/main/java/com/atayc/aktan/MainActivity.java
@@ -0,0 +1,68 @@
+package com.atayc.aktan;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView play;
+    MediaPlayer hi;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set the status bar color
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.black));
+
+        playMusic(); // Start background music
+
+        play = findViewById(R.id.play);
+        hi = MediaPlayer.create(this, R.raw.kandai);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActvity.class);
+                startActivity(intent);
+                hi.start(); // Start sound effect
+            }
+        });
+    }
+
+    private void playMusic() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.spirit);mediaPlayer.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Stop and release mediaPlayer when activity is no longer visible
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Release mediaPlayer resources when the activity is destroyed
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
